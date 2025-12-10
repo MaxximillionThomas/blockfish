d8a602cb148816d4e69e15db585e8487880176f3
Maxximillion Thomas <maxxthomas123@gmail.com>
2025-12-10 11:43:10 -0500

    Illegal move fixes
    Corrected null (illegal) move snapback, and added status update as part of each piece drop.
    
    Declared a function for updating the turn status, checking whose turn it is, whether they have legal moves, and updating both the status text and the game board accordingly, as to not allow illegal moves.
    
    Starting board reconfigured with these changes reflected, and the turn status is updated on game-start.
    
    To note that although mid-game moves have been corrected moves have been corrected (en-passant, castling), players may still move after a draw by repetition. Checkmates are fine.

--- js/main.js
+++ js/main.js
@@ -19,18 +19,59 @@ function onDrop (source, target) {
     });
 
     // If the move is illegal, return the piece to its original square
-    if (move == null) {
+    if (move === null) {
         return 'snapback';
     }
+
+    // Update the turn status text
+    updateStatus();
+}
+
+// Update the game status text
+function updateStatus() {
+  var status = '';
+
+  // Determine whose turn it is
+  var moveColor = 'White';
+  if (game.turn() === 'b') {
+      moveColor = 'Black';
+  }
+
+  // Check if a player has legal moves left
+  if (game.in_checkmate()) {
+      status = 'Game over. ' + moveColor + ' has been checkmated.';
+  } else if (game.in_draw()) {
+      status = 'Game over, the position is a draw.';
+  } else {
+      status = moveColor + ' to move.';
+      if (game.in_check()) {
+          status += ' ' + moveColor + ' is in check.';
+      }
+  }
+
+  // Update the status div
+  $('#status').html(status);
+
+  // Update the move history div
+  $('#pgn').html(game.pgn());
+}
+
+// Force the board to accurately reflect the game state
+function onSnapEnd () {
+    board.position(game.fen());
 }
 
 // Create configurations for the chessboard before it is created
 var config = {
   draggable: true,
   position: 'start',
-  onDrop: onDrop
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
 };
 
 // Initialize the chessboard (div with id 'myBoard') 
  var board = Chessboard('myBoard', config);
+
+ // Update the board status on game start
+updateStatus();
  
