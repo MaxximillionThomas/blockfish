6a6f40d2b9cbbc849411bf0cdeac4841d9b765dd
Maxximillion Thomas <maxxthomas123@gmail.com>
2025-12-11 00:21:16 -0500

    Waiting turns, Stockfish engine
    Players may no longer interact with the opponent's pieces.
    
    Saved scripts to the repository for the functionality of the auto-calculating chess move engine.
    
    Instantiated an engine object with a method for reading the board positioning to calculate the best move, and an event handler for transforming the notation data into variables usable in the pre-existing game.move() logic.

--- js/main.js
+++ js/main.js
@@ -5,14 +5,66 @@
  Date:      December 9, 2025
  */
 
+//==========  Logic  ==========
 // Establish the logic and rules of the chess game
 var game = new Chess();
 
-// Prevent interactions once the game is over
+//==========  Engine  ==========
+// Initialize the Stockfish chess engine
+var engine = new Worker('js/stockfish.js');
+
+// Set up responses from the engine
+engine.onmessage = function(event) {
+    // Engine produces many messages - we only care about 'bestmove' messages for decision making
+    if (event.data.startsWith('bestmove')) {
+        // Extract only the notation portion of the best move (ex: 'bestmove e1e3')
+        var bestMove = event.data.split(' ')[1];
+
+        // Convert the bestmove into a format the chess.js library understands
+        // 1st index is starting point, 2nd index is "ending" point (0,2 means 0-1). 
+        var source = bestMove.substring(0, 2);
+        var target = bestMove.substring(2, 4);
+        // 4th index is blank unless there is a promotion (ex: 'e7e8q' means pawn promotes to queen)
+        var promotion = bestMove.substring(4, 5);
+
+        // Use the shared game logic for the engine's move
+        game.move({
+            from: source,
+            to: target,
+            // Queen promotion as empty string protection
+            promotion: promotion || 'q'
+        });
+
+        // Update the chessboard with the engine's move
+        board.position(game.fen());
+        updateStatus();
+    }
+};
+
+// Trigger the engine to make a move
+function makeEngineMove() {
+    // Halt thinking if the game is over
+    if (game.game_over()) {
+        return;
+    }
+    // Send the current game position to the engine
+    engine.postMessage('position fen ' + game.fen());
+    // Allow 10 moves of depth to the engine
+    engine.postMessage('go depth 10');
+}
+
+//==========  Player  ==========
+// Prevent interactions once the game is over or during the opponents turn
 function onDragStart (source, piece) {
+    // Game over
     if (game.game_over()){
         return false;
     }
+    // Opponents turn
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false;
+    }   
 }
 
 // Allow piece drop interactions between chess pieces and squares
@@ -32,8 +84,12 @@ function onDrop (source, target) {
 
     // Update the turn status text
     updateStatus();
+
+    // Allow a moment before the engine makes its move
+    window.setTimeout(makeEngineMove, 250);
 }
 
+//==========  Game  ==========
 // Update the game status text
 function updateStatus() {
   var status = '';
